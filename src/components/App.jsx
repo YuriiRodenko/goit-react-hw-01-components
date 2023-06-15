7baf3018c0e6da9d09331ab635c38d9a91b1f021
YuriiRodenko <jury.rodenko@gmail.com>
2023-06-15 12:36:22 +0300

    add Profile.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import user from './profile/user.json';
+import { Profile } from './profile/profile';
+
+
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+
+        
+      />
+    
     </div>
-  );
-};
+  )
+}
